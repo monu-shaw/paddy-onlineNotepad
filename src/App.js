88ca9ea88317f605d4187aae39b2f8c03f30f0ae
monu-shaw <68293997+monu-shaw@users.noreply.github.com>
2023-08-07 21:45:35 +0530

    4 files modified

--- src/App.js
+++ src/App.js
@@ -1,33 +1,44 @@
-//import { collection, , getDocs  } from "firebase/firestore"; 
+//import { collection, , getDocs  } from "firebase/firestore";
 //import db from './firbase';
-import {BrowserRouter, Routes, Route} from "react-router-dom"
-import Login from "./components/login.jsx";
-import Navbar from "./components/navbar.jsx"
-import Paddy from './components/paddy.jsx'
-import Signup from './components/add.jsx'
-import Offline from "./offline/offline.jsx";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './components/login.jsx';
+import Navbar from './components/navbar.jsx';
+import Paddy from './components/paddy.jsx';
+import Signup from './components/add.jsx';
+import Offline from './offline/offline.jsx';
 
 import useNetwork from './offline/useNetwork';
-import Betatester from "./components/betatesterform.jsx";
-import Share from "./components/share.jsx";
-
+import Betatester from './components/betatesterform.jsx';
+import Share from './components/share.jsx';
+import React from 'react';
 
 function App() {
   const networkState = useNetwork();
-  
+
   return (
     <BrowserRouter>
-    {networkState.online && <Navbar />}
-    <Routes>
-      <Route path='/' element={<Login network={networkState.online}/>}/>
-      <Route path='/home' element={<Paddy network={networkState.online}/>}/>
-      <Route path='/share' element={<Share network={networkState.online}/>}/>
-      <Route path='/signup' element={<Signup network={networkState.online}/>}/>
-      <Route path='/offline' element={<Offline network={networkState.online}/>} />
-      <Route path='/signupasbetatester' element={<Betatester network={networkState.online}/>} />
-    </Routes>
-  </BrowserRouter>
-
+      {networkState.online && <Navbar />}
+      <Routes>
+        <Route path="/" element={<Login network={networkState.online} />} />
+        <Route path="/home" element={<Paddy network={networkState.online} />} />
+        <Route
+          path="/share"
+          element={<Share network={networkState.online} />}
+        />
+        <Route
+          path="/signup"
+          element={<Signup network={networkState.online} />}
+        />
+        <Route
+          path="/offline"
+          element={<Offline network={networkState.online} />}
+        />
+        <Route
+          path="/signupasbetatester"
+          element={<Betatester network={networkState.online} />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -6,18 +6,18 @@ import Button from '@mui/material/Button';
 import { Link, useLocation } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { logOut } from '../redux/reducers/users';
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 export default function Navbar() {
-  const logStatus = useSelector(state=> state.user.status)
+  const logStatus = useSelector((state) => state.user.status);
   const dispatch = useDispatch();
-  const [logMes , setLogMes] = useState(false);
-  const currntPosition = useLocation().pathname
-  useEffect(()=>{
-    if(currntPosition === '/signup'){
-      setLogMes(true)
-    }else{
-      setLogMes(false)
+  const [logMes, setLogMes] = useState(false);
+  const currntPosition = useLocation().pathname;
+  useEffect(() => {
+    if (currntPosition === '/signup') {
+      setLogMes(true);
+    } else {
+      setLogMes(false);
     }
   }, [currntPosition]);
   return (
@@ -27,7 +27,20 @@ export default function Navbar() {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             PADDY.IO
           </Typography>
-          { logStatus === true?<Button color="inherit" onClick={()=>dispatch(logOut())}>Logout</Button>:<Button color="inherit"><Link to={logMes?'/':"/signup"} className='text-light text-decoration-none'>{logMes?'LogIn':'SignUp'}</Link></Button>}
+          {logStatus === true ? (
+            <Button color="inherit" onClick={() => dispatch(logOut())}>
+              Logout
+            </Button>
+          ) : (
+            <Button color="inherit">
+              <Link
+                to={logMes ? '/' : '/signup'}
+                className="text-light text-decoration-none"
+              >
+                {logMes ? 'LogIn' : 'SignUp'}
+              </Link>
+            </Button>
+          )}
         </Toolbar>
       </AppBar>
     </Box>
